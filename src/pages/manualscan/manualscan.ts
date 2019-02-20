768336796945803e4f1153ebf1c7a92bcd8f26f6
Fredrick Oluoch <menty44@gmail.com>
2019-02-21 01:16:14 +0300

    manual scan now working pending adding it to cart

--- src/pages/manualscan/manualscan.ts
+++ src/pages/manualscan/manualscan.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, ToastController, NavParams } from 'ionic-angular';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { LoadingController } from 'ionic-angular';
 import swal from 'sweetalert';
+
+import axios from 'axios';
+// import { HTTP } from '@ionic-native/http';
+import {HttpHeaders} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
+import {map} from "rxjs/operators/map";
+
+import { MainPage } from '../';
 
 
 /**
@@ -21,11 +29,15 @@ export class ManualscanPage {
   public manualcode: any;
   public enteredcode: any;
   public manualresults: any;
+  public  oluoch: any;
+  public kuku: any;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               private barcodeScanner: BarcodeScanner,
-              public loadingCtrl: LoadingController) {
+              public http: HttpClient,
+              public toastCtrl: ToastController,
+                public loadingCtrl : LoadingController) {
   }
 
   static ionViewDidLoad() {
@@ -33,14 +45,55 @@ export class ManualscanPage {
   }
 
   searchProd() {
+    var loading = this.loadingCtrl.create({
+      spinner: 'hide',
+      content: 'Searching Product ...'
+  });
       const manualcode = this.manualcode;
       console.log("my searchProd" + manualcode);
 
       // this.barcodeScanner.scan().then(barcodeData => {
           console.log('Barcode data', JSON.stringify(manualcode));
-          this.enteredcode = manualcode;
-          swal("Oops!", "oh yea nigga!" + JSON.stringify(manualcode), "success");
-          this.manualresults = manualcode;
+          //this.enteredcode = manualcode;
+
+          this.http
+          .get('http://192.168.1.19:8080/biosearch?code=' + manualcode)
+          .subscribe((data : any) =>
+              {
+                  console.log(data);
+                  console.warn('response', JSON.stringify(data));
+                  console.warn('response', data.ok);
+                  //localStorage.setItem('profile', JSON.stringify(data));
+                  if(data.ok === '00') {
+                    console.warn('we are inside ', JSON.stringify(data));
+                      loading.dismiss();
+                      const toast = this.toastCtrl.create({
+                          message: 'Success ' ,
+                          position: 'bottom1',
+                          duration: 500
+                      });
+                      toast.present();
+                      //const strdata = data;
+                      const findata = JSON.parse(data);
+                      this.oluoch = findata;
+                      // this.manualresults = findata;
+                      this.kuku = 'mmmh';
+                  }
+              },
+              (error : any) =>
+              {
+                  const toast = this.toastCtrl.create({
+                      message: 'Warning Product not found ' ,
+                      position: 'Incorrect Credentials',
+                      duration: 3500
+                  });
+                  toast.present();
+                  console.dir(error);
+                  console.warn('response', JSON.stringify(error));
+              });
+
+          // swal("Oops!", "oh yea nigga!" + JSON.stringify(manualcode), "success");
+          
           // loader.dismiss();
       // }).catch(err => {
       //     this.navCtrl.push('ScanPage');
